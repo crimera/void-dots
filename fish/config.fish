b1f54547bd7c45942847394ebf1dca3ef911dee3
crimera <stevendavemiranda2@gmail.com>
2023-05-28 14:50:56 +0800

    up
    - do not cat term color if in tmux
    - added cpu and memory to status bar
    - uncommented default swayidle config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
 	# sets the color of the terminal to wal
-	cat /home/fauna/.cache/wal/sequences
+    if not string match --quiet -e $TERM 'screen-256color'
+        cat /home/fauna/.cache/wal/sequences
+    end
     # Commands to run in interactive sessions can go here
 end
